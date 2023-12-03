2f3f8df69e60b50952a6050097df16ebe31096d5
Prince Kumar <princepk7878kumar@gmail.com>
2023-12-03 15:22:30 +0530

    Added basic layput and intergrated to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
 
 
 function App() {
 
   return (
-    <>
-      <h1 className="bg-black text-white ">hii</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
   )
 }
 
